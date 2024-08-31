bf166a522b8ef0ded75300a0cf562682cfef2826
Lucas Gregory <124362765+oLucasGregory@users.noreply.github.com>
2024-08-31 12:53:42 +0100

    Configuraçoes basicas e testes

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Teste from "./test";
+import EstiloGlobal from "./styles";
+
+
+function App() {
+  return (
+    <>
+    <Teste/>
+    <EstiloGlobal/>
+    </>
+  )
+}
+
+export default App;
